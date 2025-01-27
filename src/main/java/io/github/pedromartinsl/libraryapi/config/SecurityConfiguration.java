0d42f26b96926c66d3bd416370d002b35ad52eed
Pedro Martins <pedromartinsdelemos@gmail.com>
2025-01-26 23:18:12 -0300

    Login form customization

--- src/main/java/io/github/pedromartinsl/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/pedromartinsl/libraryapi/config/SecurityConfiguration.java
@@ -16,7 +16,9 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)//proteção para as pag html, pra fazer req de forma autenticada, precisa de um token csrf no backend - desabilitar para conseguir fazer uma requisição através de outras requisições
-                .formLogin(Customizer.withDefaults())// formulário padrão
+                .formLogin(configurer -> {
+                    configurer.loginPage("/login").permitAll();
+                })// formulário padrão
                 .httpBasic(Customizer.withDefaults())// através do postman, fiormulário de login ou outras aplicações
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated()) // a req precisa estar autenticada
                 .build();
