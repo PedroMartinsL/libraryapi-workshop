0e4b6dea7020c721112d060b523e5b51b0999810
Pedro Martins <122813242+PedroMartinsL@users.noreply.github.com>
2025-02-12 15:05:14 -0300

    Users repository in memory with cryptography

--- src/main/java/io/github/pedromartinsl/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/pedromartinsl/libraryapi/config/SecurityConfiguration.java
@@ -6,6 +6,12 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -22,5 +28,27 @@ public class SecurityConfiguration {
                 .httpBasic(Customizer.withDefaults())// através do postman, fiormulário de login ou outras aplicações
                 .authorizeHttpRequests(authorize -> authorize.anyRequest().authenticated()) // a req precisa estar autenticada
                 .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10); //criptografa e produz um hash,  o10 é indicar quantas vezes passa pelo bcrypt
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails user1 = User.builder()
+            .username("usuario")
+            .password(encoder.encode("123"))
+            .roles("USER")
+            .build();
+
+        UserDetails user2 = User.builder()
+            .username("admin")
+            .password(encoder.encode("321"))
+            .roles("ADMIN")
+            .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
     }
 }
